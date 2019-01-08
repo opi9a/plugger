c2c5d028914119bf8d26ac2a8f128be2c95f643d
gav <opi9a@gavinroberts.com>
2019-01-08 10:45:15 +0000

    first working solution

--- sol2plug.py
+++ sol2plug.py
@@ -1,8 +1,8 @@
 import time
 import os
 import csv
-# import requests
-from requests_html import HTMLSession
+import requests
+# from requests_html import HTMLSession
 # from bs4 import BeautifulSoup
 
 from tplink_smartplug import SmartPlug
@@ -28,12 +28,13 @@ def main(panel_ip, socket_ip, threshold,
         print('Could not find a socket at', socket_ip)
         return 1
 
+    # get and print initialization info
     info = plug.info
     print('Found plug')
     print('- name'.ljust(pads[0]), info['alias'])
     print('- model'.ljust(pads[0]), info['model'])
     print('Initial state:'.ljust(pads[0]), "ON" if plug.is_on else "OFF")
-    print('Threshold:'.ljust(pads[0]), threshold)
+    print('Threshold set:'.ljust(pads[0]), threshold)
     print('')
 
     # initialise the log file if reqd
@@ -51,7 +52,8 @@ def main(panel_ip, socket_ip, threshold,
         print(ts, end=' ')
 
         # try to read the panel's current output
-        success, panel_output = get_panel_output(panel_ip)
+        success, panel_output = get_panel_output(panel_ip=panel_ip,
+                                                 target='OutputPower')
 
         if not success:
             print(f'failed to get panel output, error: {panel_output}')
@@ -83,7 +85,7 @@ def main(panel_ip, socket_ip, threshold,
                 log_list.append('leave off')
                 print('leave off')
 
-        # get a plug reading after any changes
+        # log a plug reading after any changes
         log_list.append(plug.is_on)
 
         # write out log 
@@ -92,31 +94,60 @@ def main(panel_ip, socket_ip, threshold,
             writer.writerow(log_list)
 
         time.sleep(interval)
+  
 
-
-
-def get_panel_output(panel_ip=None, target='target'):
+def get_panel_output(panel_ip=None, target=None):
     """Returns tuple of success flag and value.
     If success, value is the power output of the panel.
     Otherwise it is the http response.
+
+    xml_text parameter is for testing (without getting url)
     """
 
-    # get the panel web page
     url = 'http://' + panel_ip
-    print('trying url:', url)
-    session = HTMLSession()
-    r = session.get(url)
+    response = requests.get(url)
 
-    if not r.ok:
-        return False, str(r)
+    if not response.ok:
+        return False, str(response)
 
-    # render the web page - executing the js
-    r.html.render()
+    xml_text = response.text
 
-    # get the target
-    out = r.html.find(id=target)[0].text
 
-    return True, float(out) 
+    result = None
+
+    if target in xml_text:
+        result = xml_text.split(target)[1][1:-2]
+    else:
+        print(f"Can't find {target} in xml")
+        return False, f'"{target}" not found in xml'
+
+    return True, float(result) 
+
+
+
+# def get_panel_output(panel_ip=None, target='target'):
+#     """Returns tuple of success flag and value.
+#     If success, value is the power output of the panel.
+#     Otherwise it is the http response.
+#     """
+
+#     # get the panel web page
+#     url = 'http://' + panel_ip
+#     print('trying url:', url)
+#     session = HTMLSession()
+#     r = session.get(url)
+
+#     if not r.ok:
+#         return False, str(r)
+
+#     # render the web page - executing the js
+#     r.html.render()
+
+#     # get the target
+#     out = r.html.find('#' + target)[0]
+#     print('out', out)
+
+#     return True, float(out.text) 
 
 
 # def get_panel_output(panel_ip=None, target='target'):
@@ -137,4 +168,5 @@ def get_panel_output(panel_ip=None, target='target'):
 #     out = soup.find(id=target).text
 
 #     return True, float(out) 
+
 
